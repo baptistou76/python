157b4a8a96492ebc48a548eeae9cd72c35c6fabe
Baptiste Caumont <bcaumont@c2r4p1.42lehavre.fr>
2026-03-13 18:06:28 +0100

    piscine

--- piscine_python/py_data_0/ex05/building.py
+++ piscine_python/py_data_0/ex05/building.py
@@ -1,6 +1,51 @@
 import sys
+import string
+
+
+def Analyzer(text):
+
+    """
+    Count different types of characters in the text.
+    """
+    upper = 0
+    lower = 0
+    punctuation = 0
+    spaces = 0
+    digits = 0
+
+    for c in text:
+        if c.isupper():
+            upper += 1
+        elif c.islower():
+            lower += 1
+        elif c.isdigit():
+            digits += 1
+        elif c.isspace():
+            spaces += 1
+        elif c in string.punctuation:
+            punctuation += 1
+    print(f"The text contains {len(text)} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punctuation} punctuations marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
+
 
 def main():
-    if len(sys.argv) != 2:
-        exit(0)
-    
+    """
+    Main program.
+    """
+    if len(sys.argv) > 2:
+        print("AssertionError: more than one argument is provided")
+        exit(1)
+    if len(sys.argv) == 2:
+        text = sys.argv[1]
+    else:
+        print("What is the text to count?")
+        text = sys.stdin.read()
+    Analyzer(text)
+
+
+if __name__ == "__main__":
+    main()
