09497fb0faed1858986b6d2b839aa46091b96515
Baptiste Caumont <bcaumont@c2r2p1.42lehavre.fr>
2026-03-17 15:27:45 +0100

    python

---
+++ piscine_python/Module_01/ex00/give_bmi.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    """
+    Check if the values are int or float and calculate the bmi.
+    Return a bmi array
+    """
+    if len(height) != len(weight):
+        raise ValueError("Lists must be the same size")
+    for h, w in zip(height, weight):
+        if not isinstance(h, (int, float)) or not isinstance(w, (int, float)):
+            raise TypeError("All values must be int or float")
+    h = np.array(height)
+    w = np.array(weight)
+    return (w / (h ** 2)).tolist()
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Check the limit passed as parameter and return true or false
+    """
+    arr = np.array(bmi)
+    return(arr > limit).tolist()
