a75c243540b7a009cd64d6c67f7511f588bd6e10
Baptiste Caumont <bcaumont@c2r2p1.42lehavre.fr>
2026-03-20 15:05:58 +0100

    piscine

---
+++ piscine_python/Module02/ex00/load_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    try:
+        if not isinstance(path, str):
+            print("Error: Path must be a string")
+            return None
+        df = pd.read_csv(path)
+        if df.empty:
+            print("Error: empty csv file")
+            return None
+        if df.isnull().all().all():
+            print("Error: csv contains only null values")
+            return None
+        if df.shape[1] < 2:
+            print("Error: not enough columns for a csv")
+            return None
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except Exception as e:
+        print("Error:", e)
+        return None
